f2f228ff39cfcaaa95fed8fae26daeeb5a97731b
mirza-sync <mirzasyahmi123@gmail.com>
2022-11-28 00:03:51 +0800

    Init firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
 import './App.css';
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA8LH5sMT-UhFk7aIBUp_S3bH4IxB8pypI",
+  authDomain: "superchat-90289.firebaseapp.com",
+  projectId: "superchat-90289",
+  storageBucket: "superchat-90289.appspot.com",
+  messagingSenderId: "608476507437",
+  appId: "1:608476507437:web:5ac54814101f62827bf7a4",
+  measurementId: "G-VCZVRL3HF8"
+})
 
 function App() {
   return (
